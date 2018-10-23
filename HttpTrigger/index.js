5810501d2e5f79d18c95421caedba3454b71d525
baemins <mib@microsoft.com>
2018-10-23 11:21:01 +0900

    Get

---
+++ HttpTrigger/index.js
@@ -0,0 +1,17 @@
+module.exports = function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    if (req.query.name || (req.body && req.body.name)) {
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: "The product name for your product id " + (req.query.name || req.body.name) + "is Starfruit Explosion"
+        };
+    }
+    else {
+        context.res = {
+            status: 400,
+            body: "Please pass a name on the query string or in the request body"
+        };
+    }
+    context.done();
+};
